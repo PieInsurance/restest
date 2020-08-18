e31034c0e0335a3226f114416bee13c8debfc4bd
Martin Kreibe <mkreibe@users.noreply.github.com>
2020-08-18 11:31:47 -0600

    initial

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# restest
-A test REST API
+# Restest
+
+A test REST API.
+
+## Setup
+
+This code uses [Go](https://golang.org/) and you are expected to [download](https://golang.org/dl/) and run it locally.
+
+To run the application, run this command:
+
+```
+go run main.go
+```
+
+this will stand up a service on port `8000`.
+
+## Assignment
+
+We want to test the functionality of our [temperature](https://en.wikipedia.org/wiki/Temperature) API. We _think_ it
+works well, but need more system level testing. Use [Postman](https://www.postman.com/) to make test cases for this API.
+
+## Examples:
+
+These examples are from a [well known table](https://en.wikipedia.org/wiki/Temperature#Examples) and work.
+
+```http://localhost:8000/1811```
+
+```
+[
+    "1.811000E+00 kK",
+    "1.537850e+03 °C",
+    "1.600221E+03 nm"
+]
+```
+
+```http://localhost:8000/373.1339```
+
+```
+[
+    "3.731339E-01 kK",
+    "9.998390e+01 °C",
+    "7.766649E+03 nm"
+]
+```
+
+```http://localhost:8000/16E6```
+
+```
+[
+    "1.600000E+01 MK",
+    "1.599973e+07 °C",
+    "1.811250E-01 nm"
+]
+```
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// _prefixes we want to use.
+var _prefixes = map[string]int{
+	"T": 12,
+	"G": 9,
+	"M": 6,
+	"":  0,
+	"k": 3,
+	"n": -9,
+}
+
+// errResponse returns a nice error/
+func errResponse(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Header().Add("Content-Type", "text/text")
+	_, _ = w.Write([]byte(err.Error()))
+
+	fmt.Printf("errored: %s\n", err)
+}
+
+// prefix encodes an SI prefix to the value and applies it to the unit.
+func prefix(value float64, unit string) string {
+
+	prefix := ""
+	order := math.Log10(value)
+
+	for pre, o := range _prefixes {
+		if math.Abs(order-float64(_prefixes[prefix])) > math.Abs(order-float64(o)) {
+			prefix = pre
+		}
+	}
+
+	exp := _prefixes[prefix]
+	short := value / math.Pow10(exp)
+
+	return fmt.Sprintf("%.6E %s%s", short, prefix, unit)
+}
+
+// tempHandler decodes the path and returns an appropriate response.
+func tempHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	kelvinStr := vars["temp"]
+
+	kelvin, err := strconv.ParseFloat(kelvinStr, 64)
+	if err != nil {
+		errResponse(w, err)
+		return
+	}
+
+	fmt.Printf("running: %f\n", kelvin)
+
+	celsius := kelvin - 273.15
+	λ := 0.002898 / kelvin
+
+	values := []string{
+		prefix(kelvin, "K"),
+		fmt.Sprintf("%e °C", celsius),
+		prefix(λ, "m"),
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(values)
+}
+
+// main entry point.
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc(`/{temp}`, tempHandler)
+	router.HandleFunc(`/{temp:[+-]?\d*\.?\d+}`, tempHandler)
+	router.HandleFunc(`/{temp:[+-]?\d*\.?\d+[Ee][+-]?\d+}`, tempHandler)
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
